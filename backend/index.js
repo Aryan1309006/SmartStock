305af012c80a0d946ec10fbd35cd7a489891d921
Aryan <shitalaryan333@gmail.com>
2026-09-19 17:09:45 +0530

    public server added

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const startServer = async () => {
   try {
     await connectToDB();
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
+    app.listen(PORT,"0.0.0.0", () => {
       console.log(`server is running on port ${PORT}`);
     });
   } catch (error) {
